8d16cbfaca481890171306a56fc342c2f1d48517
Bartek Svaberg <bartek.svaberg@gmail.com>
2025-04-03 14:31:52 +0200

    Add docker compose script

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-docker stop process-matches-service
-docker rm process-matches-service
-docker build -t process-matches-app .
-docker run --name process-matches-service --network fogis-network -v process-matches-data:/data process-matches-app
+docker compose watch
